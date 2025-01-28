4ec9f8d29c8c8e98e4ebc470b2e0057df9059693
umar <mdumarkhan.1207@gmail.com>
2025-01-29 00:44:40 +0530

    update: milap api integration

---
+++ src/app/api/scrape/route.js
@@ -0,0 +1,16 @@
+import puppeteer from "puppeteer"
+
+export async function GET(req) {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto("https://milaap.org/fundraisers/support-us-to-participate-in-singapore-auv-challenge-2025")
+    const amount = await page.$("#donate-section > div.sticky-progress-section > div.amount-section > div.raised-amount > div.amount")
+    const amountText = await amount.evaluate(el => el.textContent)
+
+    await browser.close()
+
+    const amountNumber = amountText.replace(/[^0-9]/g, '')
+
+    return new Response(amountNumber, { status: 200 })
+}
